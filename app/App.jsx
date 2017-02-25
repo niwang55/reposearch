05770ee9d17524bf3c92ec9de061b8af959a6b8a
Nicholas Wang <nickwang55@gmail.com>
2017-02-24 17:57:24 -0800

    Implement similar followers feature

--- app/App.jsx
+++ app/App.jsx
@@ -6,9 +6,13 @@ export default class App extends React.Component {
 
     this.state = {
       username: '',
-      user: null,
+      userInfo: null,
+      userFollowers: null,
       repos: null,
-      filteredRepos: null
+      filteredRepos: null,
+      secondUsername: '',
+      secondUserFollowers: null,
+      similarFollowers: null
     };
   }
 
@@ -20,6 +24,7 @@ export default class App extends React.Component {
 
   handleUserSubmit(e) {
     e.preventDefault();
+
     const repoUrl = `https://api.github.com/users/${this.state.username}/repos`;
     const userUrl = `https://api.github.com/users/${this.state.username}`;
 
@@ -28,7 +33,7 @@ export default class App extends React.Component {
       .then(userInfo => {
         console.log(userInfo);
         this.setState({
-          user: userInfo
+          userInfo: userInfo
         });
       });
 
@@ -51,6 +56,52 @@ export default class App extends React.Component {
     });
   }
 
+  handleSecondUserChange(e) {
+    this.setState({
+      secondUsername: e.target.value
+    });
+  }
+
+  handleSecondUserSubmit(e) {
+    e.preventDefault();
+
+    const firstUserFollowerUrl = `https://api.github.com/users/${this.state.username}/followers`;
+    const secondUserFollowerUrl = `https://api.github.com/users/${this.state.secondUsername}/followers`;
+
+    fetch(firstUserFollowerUrl)
+      .then(blob => blob.json())
+      .then(userFollowers => {
+        this.setState({
+          userFollowers: userFollowers
+        });
+        return fetch(secondUserFollowerUrl);
+      })
+      .then(blob => blob.json())
+      .then(secondUserFollowers => {
+        this.setState({
+          secondUserFollowers: secondUserFollowers
+        });
+
+        this.compareFollowers();
+      });
+  }
+
+  compareFollowers() {
+    let similarArray = [];
+
+    this.state.userFollowers.forEach(userFollower => {
+      this.state.secondUserFollowers.forEach(secondUserFollower => {
+        if (userFollower.id === secondUserFollower.id) {
+          similarArray.push(userFollower.login);
+        }
+      });
+    });
+
+    this.setState({
+      similarFollowers: similarArray
+    });
+  }
+
   render() {
     return (
       <div>
@@ -61,22 +112,44 @@ export default class App extends React.Component {
 
         <div>
 
-          {this.state.user && 
+          {this.state.userInfo && 
             <div className='user-banner'>
-              <h1>{this.state.user.name}</h1>
-              <img className='avatar-img' src={this.state.user.avatar_url} />
+              <h1>{this.state.userInfo.name}</h1>
+              <img className='avatar-img' src={this.state.userInfo.avatar_url} />
             </div>
           }
 
           {this.state.repos &&
             <div>
-              <h3>Repositories</h3>
-              <form>
-                <input type='search' onChange={this.handleRepoChange.bind(this)} placeholder='filter by repo name' className='search-bar' id='repo-search' />
-              </form>
-              {this.state.filteredRepos.map((repo, index) => (
-                <Repo key={index} details={repo} />
-              ))}
+
+              <div>
+                <h3>Repositories</h3>
+                <form>
+                  <input type='search' onChange={this.handleRepoChange.bind(this)} placeholder='filter by repo name' className='search-bar' id='repo-search' />
+                </form>
+                {this.state.filteredRepos.map((repo, index) => (
+                  <Repo key={index} details={repo} />
+                ))}
+              </div>
+
+              <div>
+                <h3>Search another user to find common followers</h3>
+                <form onSubmit={this.handleSecondUserSubmit.bind(this)}>
+                  <input onChange={this.handleSecondUserChange.bind(this)} type='search' className='search-bar' placeholder='search another user' />
+                  <input type='submit' />
+                </form>
+
+                {this.state.similarFollowers &&
+                  <div>
+                    <h4>Same Followers</h4>
+                    {this.state.similarFollowers.map((follower, index) => (
+                      <Follower key={index} name={follower} />
+                    ))}
+                  </div>
+                }
+
+              </div>
+
             </div>
           }
 
@@ -86,16 +159,21 @@ export default class App extends React.Component {
   }
 }
 
-class Repo extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+const Repo = (props) => {
+  return (
+    <div>
+      {props.details.name} <a href={props.details.html_url}>Link</a>
+    </div>
+  );
+};
 
-  render() {
-    return (
-      <div>
-        {this.props.details.name} <a href={this.props.details.url}>Link</a>
-      </div>
-    );
-  }
-}
+const Follower = (props) => {
+  return (
+    <div>
+      {props.name}
+    </div>
+  );
+};
+
+
+
